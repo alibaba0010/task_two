71f01c7a5690b80607e9a6be601b97082448c203
Zakariyah Ali <zakariyahali100@gmail.com>
2024-12-06 04:11:24 +0000

    commit

--- zkpass-capstone/src/vite-env.d.ts
+++
@@ -1,9 +0,0 @@
-/// <reference types="vite/client" />
-
-import type { Eip1193Provider } from "ethers/providers";
-
-declare global {
-	interface Window {
-		ethereum?: Eip1193Provider | null;
-	}
-}
